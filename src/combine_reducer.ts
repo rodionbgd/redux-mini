cb60c0e8ee077e39a324c79bff58fa3bb065a2bb
rodionbgd <rodionbgd@mail.ru>
2021-11-28 13:04:24 +0300

    Добавлен prettier

--- src/combine_reducer.ts
+++ src/combine_reducer.ts
@@ -6,6 +6,7 @@ export default function combineReducer<S, A>(
   return function reducer(state: S, action: A): S {
     const newState = {} as S;
     Object.keys(reducers).forEach((prop) => {
+      // FIXME: remove any
       (newState as any)[prop] = reducers[prop]((state as any)[prop], action);
     });
     return newState;
